9e9ab65ec96c994a774563901a7f06f5fa474d0b
thinksalot <thapa.aalok@gmail.com>
2013-10-19 22:07:03 +0545

    loading skype to workspace

--- xmonad.hs
+++ xmonad.hs
@@ -14,13 +14,13 @@ import XMonad.Hooks.SetWMName			-- Java window drawing bug
 myTerminal="gnome-terminal"
 
 -- workspaces
-myWorkspaces=["1:web","2:skype","3:vim","4:command","5:filemanager","6:firefox","7:extra1","8:extra2","9:extra3"]
+myWorkspaces=["1:web","2:vim","3:skype","4:command","5:filemanager","6:firefox","7:extra1","8:extra2","9:extra3"]
 
 myManageHooks=composeAll.concat $
 	[
 		[className=? "Google-chrome" --> doShift "1:web"],
 
-	 	[className=? "Skype" --> doShift "2:skype"],
+	 	[className=? "Skype" --> doShift "3:skype"],
 
 	 	[title=? "Syncro SVN Client" --> doShift "4:command"],
 
